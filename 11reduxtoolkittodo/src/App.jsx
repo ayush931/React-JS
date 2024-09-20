9db5c6283a0146db016d0345950f7bcef2d8c9ac
ayush931 <ayushkumar9315983@gmail.com>
2024-09-20 21:41:19 +0530

    Redux toolkit

---
+++ 11reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './components/AddTdod'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <h1>Learning redux toolkit</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
