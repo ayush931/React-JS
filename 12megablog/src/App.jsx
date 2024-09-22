948113997de8190bd4ecc1b314ab0e9034b99e02
ayush931 <ayushkumar9315983@gmail.com>
2024-09-22 18:52:25 +0530

    .ENV Setup

---
+++ 12megablog/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.REACT_APP_APPWRITE_URL)
+
+  return (
+    <>
+      <h1>A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
