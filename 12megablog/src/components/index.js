bbe360ba75be57c5b9996ea1d8d264b816682be3
ayush931 <ayushkumar9315983@gmail.com>
2024-09-26 13:44:24 +0530

    React Button, Logout Components

--- 12megablog/src/components/index.js
+++ 12megablog/src/components/index.js
@@ -1,7 +1,13 @@
 import Header from '../components/Header/Header.jsx'
 import Footer from '../components/Footer/Footer.jsx'
+import Conatiner from '../container/Conatiner.jsx'
+import Logo from './Logo.jsx'
+import LogoutBtn from './Header/LogoutBtn.jsx'
 
 export {
     Header,
-    Footer
+    Footer,
+    Conatiner,
+    Logo,
+    LogoutBtn
 }
